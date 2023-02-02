2d1365a4afb5bb5c3558de91bba5c9c8996e2b3b
Chris Marabate <cmarabate@gmail.com>
2023-02-02 15:03:49 -0500

    🤞

--- remix.config.js
+++ remix.config.js
@@ -5,6 +5,11 @@ const { flatRoutes } = require('remix-flat-routes')
 
 module.exports = {
 	cacheDirectory: './node_modules/.cache/remix',
+  serverBuildTarget: "netlify",
+  server:
+    process.env.NETLIFY || process.env.NETLIFY_LOCAL
+      ? "./server.js"
+      : undefined,
 	ignoredRouteFiles: ['**/*'],
 
 	routes: async (defineRoutes) => {
